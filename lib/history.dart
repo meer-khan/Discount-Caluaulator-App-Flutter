a0d308d3cf409bad855b796f2e13e75bbc1f9d95
meer-khan <shahmirkhan519@gmail.com>
2021-01-15 18:56:56 +0500

    near to final submissions

--- lib/history.dart
+++ lib/history.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// import 'package:hexcolor/hexcolor.dart';
 
 class History extends StatefulWidget {
   @override
@@ -11,69 +12,128 @@ class _HistoryState extends State<History> {
   // }
   static const routeName = '/history';
 
+  Color myhex = Color(0xff123456);
+
   @override
   Widget build(BuildContext context) {
     final List args = ModalRoute.of(context).settings.arguments;
 
-    _listview(args) {
+    List<Widget> _listview(args) {
+      List<Widget> listOfwidgets = [];
       for (var i = 0; i < args.length; i++) {
         print("Prices: ${args[i]["Price"]} \n");
-      }
-      return;
-    }
-
-    return Scaffold(
-      appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Row(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  width: 1,
-                  color: Colors.black,
-                ),
-              ),
-              width: 365,
-              height: MediaQuery.of(context).size.height,
-              child: SingleChildScrollView(
-                child: Column(
+        if (args == null) {
+          return null;
+        } else {
+          listOfwidgets.add(
+            Row(
+              children: [
+                Column(
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'History',
-                          style: TextStyle(
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        RaisedButton(onPressed: () {
-                          _listview(args);
-                        }),
-                      ],
+                    Text(
+                      "Prices: ${args[i]["Price"]}Rs    Discount: ${args[i]["Discount"]}% \n Discounted Price: ${args[i]["Discounted Price"]}Rs \n Saved: ${args[i]["Saved"]}Rs",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
                     ),
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      decoration: BoxDecoration(color: Colors.amber),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          "Prices: ${args[0]["Price"]}Rs    Discount: ${args[0]["Discount"]}% \n Discounted Price: ${args[0]["Discounted Price"]}Rs \n Saved: ${args[0]["Saved"]}Rs",
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Divider(
+                      height: 10,
+                      thickness: 00,
+                      color: Colors.black,
                     )
                   ],
                 ),
-              ),
+              ],
             ),
+          );
+        }
+      }
+      return listOfwidgets;
+    }
+
+    clear() {
+      print("before : $args");
+      args.clear();
+      this.setState(() {});
+
+      print("after : $args");
+    }
+
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [
+            FloatingActionButton(
+              onPressed: clear,
+              child: Text(
+                "Clear",
+                style: TextStyle(
+                  fontSize: 10,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
           ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Row(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 1,
+                    color: Colors.black,
+                  ),
+                ),
+                width: 365,
+                height: MediaQuery.of(context).size.height,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'History',
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(color: Color(0xffb74093)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          // child: Text(
+                          //   "Prices: ${args[0]["Price"]}Rs    Discount: ${args[0]["Discount"]}% \n Discounted Price: ${args[0]["Discounted Price"]}Rs \n Saved: ${args[0]["Saved"]}Rs",
+                          //   style: TextStyle(
+                          //     fontSize: 18,
+                          //     fontWeight: FontWeight.bold,
+                          //   ),
+                          // ),
+
+                          child: Column(
+                            children: _listview(args),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // initialRoute: '/',
       routes: {
+        // '/': (context) => (MyHomePage()),
         '/history': (context) => (History()),
       },
       debugShowCheckedModeBanner: false,
@@ -50,6 +52,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
+    print("yolo: $listOfDiscounts");
   }
 
   @override
@@ -89,241 +92,245 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: [
-          FlatButton(
-            onPressed: () {
-              // Navigator.pushNamed(context, '/history',
-              //     arguments: (listOfDiscounts)  );
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          actions: [
+            FlatButton(
+              onPressed: () {
+                // Navigator.pushNamed(context, '/history',
+                //     arguments: (listOfDiscounts)  );
 
-              Navigator.pushNamed(context, "/history",
-                  arguments: listOfDiscounts);
-            },
-            child: Text(
-              'History',
-              style: TextStyle(
-                fontSize: 18,
+                Navigator.pushNamed(context, "/history",
+                    arguments: listOfDiscounts);
+              },
+              child: Text(
+                'History',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
               ),
             ),
-          ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: SingleChildScrollView(
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Material(
-                elevation: 10.0,
-                child: TextField(
-                  controller: priceController,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(90), //Not Working
-                    ),
-                    hintText: "Enter Price",
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blue)),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black),
-                    ),
-                  ),
-                  // onChanged: (text) {
-                  //   totalprice = text;
-                  // },
-                ),
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              Material(
-                elevation: 10.0,
-                child: TextField(
-                  controller: discountController,
-                  keyboardType: TextInputType.number,
-                  style: TextStyle(),
-                  decoration: InputDecoration(
-                      hintText: "Enter Discount",
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blue),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: SingleChildScrollView(
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Material(
+                  elevation: 10.0,
+                  child: TextField(
+                    controller: priceController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(90), //Not Working
                       ),
+                      hintText: "Enter Price",
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue)),
                       enabledBorder: OutlineInputBorder(
                         borderSide: BorderSide(color: Colors.black),
-                      )),
-                  // onChanged: (text) {
-                  //   print("price: controller:  ${priceController.text}");
-                  //   discount = text;
-                  // },
-                ),
-              ),
-              SizedBox(
-                height: 25.0,
-              ),
-              ButtonTheme(
-                minWidth: 120,
-                height: 40,
-                child: RaisedButton(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Text(
-                    'Discount',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                  elevation: 10.0,
-                  textColor: Colors.white,
-                  color: Colors.purpleAccent,
-                  onPressed: () {
-                    print("price controller: ${priceController.text}");
-                    showTotalPrice = priceController.text;
-                    showDiscount = discountController.text;
-
-                    discountprice = _calculate(
-                        priceController.text, discountController.text);
-
-                    saved = _calculateAmountSaved(
-                        priceController.text, discountprice);
-                    priceController.clear();
-                    discountController.clear();
-                    print(priceController.text);
-                    setState(() {});
-                  },
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                width: 370.0,
-                height: 180,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    width: 1,
-                    color: Colors.black,
-                  ),
-                ),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Calculations",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 25,
-                          ),
-                        ),
-                      ],
+                      ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
+                    // onChanged: (text) {
+                    //   totalprice = text;
+                    // },
+                  ),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Material(
+                  elevation: 10.0,
+                  child: TextField(
+                    controller: discountController,
+                    keyboardType: TextInputType.number,
+                    style: TextStyle(),
+                    decoration: InputDecoration(
+                        hintText: "Enter Discount",
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black),
+                        )),
+                    // onChanged: (text) {
+                    //   print("price: controller:  ${priceController.text}");
+                    //   discount = text;
+                    // },
+                  ),
+                ),
+                SizedBox(
+                  height: 25.0,
+                ),
+                ButtonTheme(
+                  minWidth: 120,
+                  height: 40,
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Text(
+                      'Discount',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    elevation: 10.0,
+                    textColor: Colors.white,
+                    color: Colors.purpleAccent,
+                    onPressed: () {
+                      print("price controller: ${priceController.text}");
+                      showTotalPrice = priceController.text;
+                      showDiscount = discountController.text;
+
+                      discountprice = _calculate(
+                          priceController.text, discountController.text);
+
+                      saved = _calculateAmountSaved(
+                          priceController.text, discountprice);
+                      priceController.clear();
+                      discountController.clear();
+                      print(priceController.text);
+                      setState(() {});
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: 370.0,
+                  height: 180,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 1,
+                      color: Colors.black,
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Price: " +
-                                    (showTotalPrice ==
-                                            "" // Ambigious logic but working
-                                        ? ""
-                                        : "$showTotalPrice Rs"),
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w600,
-                                  fontSize: 18,
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Discount: " +
-                                    (showDiscount == ""
-                                        ? " "
-                                        : "$showDiscount%"),
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w600,
-                                  fontSize: 18,
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Text(
-                                discountprice == null
-                                    ? "Discounted Price: "
-                                    : "Discounted Price: $discountprice",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w600,
-                                  fontSize: 18,
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Text(
-                                saved == null ? "Saved: " : "Saved Rs: $saved",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w600,
-                                  fontSize: 18,
-                                ),
-                              ),
-                            ],
+                          Text(
+                            "Calculations",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25,
+                            ),
                           ),
                         ],
                       ),
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              ButtonTheme(
-                minWidth: 130,
-                height: 40,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: RaisedButton(
-                  onPressed: () {
-                    print("List before call: $listOfDiscounts");
-                    _savedButton(
-                        showTotalPrice, showDiscount, discountprice, saved);
-                  },
-                  color: Colors.amber,
-                  child: Text(
-                    'Save',
-                    style: TextStyle(fontSize: 18),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Price: " +
+                                      (showTotalPrice ==
+                                              "" // Ambigious logic but working
+                                          ? ""
+                                          : "$showTotalPrice Rs"),
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Discount: " +
+                                      (showDiscount == ""
+                                          ? " "
+                                          : "$showDiscount%"),
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text(
+                                  discountprice == null
+                                      ? "Discounted Price: "
+                                      : "Discounted Price: $discountprice",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text(
+                                  saved == null
+                                      ? "Saved: "
+                                      : "Saved Rs: $saved",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
                   ),
-                  elevation: 10.0,
-                  textColor: Colors.white,
                 ),
-              )
-            ],
+                SizedBox(
+                  height: 20,
+                ),
+                ButtonTheme(
+                  minWidth: 130,
+                  height: 40,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: RaisedButton(
+                    onPressed: () {
+                      print("List before call: $listOfDiscounts");
+                      _savedButton(
+                          showTotalPrice, showDiscount, discountprice, saved);
+                    },
+                    color: Colors.amber,
+                    child: Text(
+                      'Save',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    elevation: 10.0,
+                    textColor: Colors.white,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
+        // This trailing comma makes auto-formatting nicer for build methods.
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
